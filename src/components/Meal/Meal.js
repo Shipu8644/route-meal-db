5bb4709af28459a4e7fba2163b42d0924be9c0ea
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-01 03:05:37 +0600

    done Alh

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Meal = (props) => {
-    const { idMeal, strMeal, strArea, strInstructions, strMealThumb } = props.meal;
-    const ins = strInstructions.slice(0, 200);
+    const { idMeal, strMeal, strMealThumb } = props.meal;
+
     return (
-        <div className='class="max-w-sm rounded-lg overflow-hidden shadow-2xl bg-purple-200' >
-            <img className='p-8 pb-1  h-96 ' src={strMealThumb} alt="" />
+        <div className='class="max-w-sm rounded-lg overflow-hidden shadow-2xl bg-purple-200 flex flex-col items-center' >
+            <img className=' p-2 w-4/5  h-64 ' src={strMealThumb} alt="" />
             <div className='px-6 py-4 text-center text-2xl'>
                 <h1>{idMeal}</h1>
                 <h2 className='font-semibold'>{strMeal}</h2>
-                {/* <h2>{strArea}</h2>
-                <p>{ins}...</p> */}
-
                 <br />
 
                 <NavLink className='p-2 border-2 bg-purple-400 font-semibold text-2xl rounded-lg' to={`/meal/${idMeal}`}>Details</NavLink>
--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -1,32 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const MealDetails = () => {
     const { mealId } = useParams();
-    console.log(mealId);
 
-    const { meal, setMeal } = useState({});
+    const [meal, setMeal] = useState({});
+
+    const history = useHistory();
+
+    const handleAllItems = () => {
+        history.push('/restaurant');
+    }
 
     useEffect(() => {
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
             .then(res => res.json())
-            .then(data => console.log(data.meals[0]))
-    }, [])
+            .then(data => setMeal(data.meals[0]))
+    }, [mealId])
+    const { strMealThumb, strCategory, strArea, strMeal, strInstructions, strTags } = meal;
+    // let ins = strInstructions.substring(0, 300);
     return (
         <div>
             <div>
                 <h1 className='text-center text-2xl font-bold font-mono'>Here is the detail below:</h1>
             </div>
 
-            <div class="flex items-center p-4 bg-white border-2 
-            border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 w-2/3 ml-48 mt-4">
-                <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
-                <div id="body" class="flex flex-col ml-5">
-                    <h4 id="name" class="text-xl font-semibold mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
-                    <p id="job" class="text-gray-800 mt-2">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <div class="flex mt-5">
-                        <img alt="avatar" class="w-6 rounded-full border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
-                        <p class="ml-3">John Doe</p>
+            <div className="flex items-center p-4 bg-white border-2 
+            border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 w-2/3 ml-48 mt-4 ">
+                <img alt="mountain" className="w-1/3  rounded-md border-2 border-gray-300 shadow-2xl" src={strMealThumb} />
+                <div id="body" className="flex flex-col ml-5">
+                    <h4 id="name" className="text-xl font-semibold mb-2">{strMeal}, {strCategory}, {strArea} [{strTags}] </h4>
+                    <h4 className='text-xl font-semibold'>Recipe:</h4>
+                    <p id="job" className="text-gray-800 mt-2">
+                        {strInstructions}</p>
+                    <div className="flex mt-3">
+                        <button onClick={handleAllItems} className='bg-purple-400 border-2 rounded-lg text-xl p-2 pl-3 mt-4'>All items</button>
                     </div>
                 </div>
             </div>
